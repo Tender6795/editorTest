c6a01d090c3a80a208344fd88cf8e0873e06e484
Muchnik Illia <muchnik.illia.js@gmail.com>
2023-02-22 21:27:31 +0200

    Add auth logic

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -1,16 +1,50 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const ApiError = require('../error/ApiError')
+const { User } = require('../models/models')
 
+const generateJwt = (id, email) => {
+  return jwt.sign({ id, email }, process.env.SECRET_KEY, {
+    expiresIn: '24h',
+  })
+}
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password } = req.body
 
-  async login(req, res) {}
+    if (!email || !password)
+      return next(ApiError.badRequest('Wrong email or password'))
+
+    const candidate = await User.findOne({ where: { email } })
+    if (candidate)
+      return next(ApiError.badRequest('User with this email already exists'))
+
+    const hashPassword = await bcrypt.hash(password, 5)
+    const { id } = await User.create({ email, password: hashPassword })
+    const token = generateJwt(id, email)
+    return res.json({ token })
+  }
+
+  async login(req, res, next) {
+    const { email, password } = req.body
+
+    if (!email || !password)
+      return next(ApiError.badRequest('Wrong email or password'))
+
+    const user = await User.findOne({ where: { email } })
+    if (!user) return next(ApiError.internal('User not found'))
+
+    const comparePassword = bcrypt.compareSync(password, user.password)
+    if (!comparePassword)
+      return next(ApiError.internal('Wrong email or password'))
+
+    const token = generateJwt(user.id, user.email)
+    return res.json({ token })
+  }
 
   async check(req, res, next) {
-    const { id } = req.query
-    if (!id) {
-      return next(ApiError.badRequest('no id'))
-    }
-    res.json(id)
+    const token = generateJwt(req.user.id, req.user.email)
+    return res.json({ token })
   }
 }
 
