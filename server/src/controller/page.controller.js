17b055be92e553eec4cee12890964981f39b4963
Muchnik Illia <muchnik.illia.js@gmail.com>
2023-02-23 10:07:41 +0200

    Fix page logic

--- server/src/controller/page.controller.js
+++ server/src/controller/page.controller.js
@@ -4,15 +4,17 @@ const ApiError = require('../error/ApiError')
 class PageController {
   async create(req, res, next) {
     const { text } = req.body
+
     if (!text) {
       return next(ApiError.badRequest('No text'))
     }
-    const page = await Page.create({ text })
+
+    const page = await Page.create({ text, userId: req.user.id })
     return res.json(page)
   }
 
   async getAll(req, res) {
-    const pages = await Page.findAll()
+    const pages = await Page.findAll({ where: { userId: req.user.id } })
     return res.json(pages)
   }
 }
--- server/src/routes/page.routes.js
+++ server/src/routes/page.routes.js
@@ -1,9 +1,11 @@
 const Router = require('express')
 const router = new Router()
 const pageController = require('../controller/page.controller')
+const authMiddleware = require('../midleware/auth.middleware')
 
-router.post('/', pageController.create)
-router.get('/', pageController.getAll)
+
+router.post('/',authMiddleware, pageController.create)
+router.get('/',authMiddleware, pageController.getAll)
 
 
 module.exports = router
